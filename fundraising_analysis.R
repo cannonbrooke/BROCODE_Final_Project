f0ee98fd44f3df3617d514033435987469ccd8fc
Cannon Brooke <cannonbrooke@protonmail.ch>
2021-11-19 20:23:36 -0800

    Update fundraising_analysis.R
    added code for cleaning DOB and adding birth dates for donor and constituents

--- fundraising_analysis.R
+++ fundraising_analysis.R
@@ -1,16 +1,34 @@
-library(here)
-library(tidyverse)
-library(ggplot2)
-library(dplyr)
+#pacman is a package mangement tool, this function load and installs all necesary packages
+pacman::p_load(tidyverse, lubridate, dplyr, 
+               ggthemes, pastecs, 
+               gridExtra, knitr, kableExtra, 
+               ggmap, fastDummies, plotly)
 
-#loading in data
-donors_org <- read.csv(here::here("clean data", "group_project_donorsredacted.csv"))
+#The lubridate package makes it easier to work with dates and times in R.
 
-glimpse(donors_org)
+#setting working directory 
+setwd("C:/Users/cabrooke/Documents/R/696/group project/Group Project/clean data")
 
-donors_org %>% filter(Donor.Segment == "Lost Donor")
+#Reading the data in and doing minor initial cleaning in the function call
+#reproducible data analysis should really avoid all automatic string to factor conversions.
+data <- read.csv("donor_data.csv",
+                 stringsAsFactors = FALSE,
+                 strip.white = TRUE,
+                 na.strings = "")
 
-ggplot(data = donors_org, aes(x = Total.Giving.Years, y = Total.Giving.Years)) +
-  geom_point()
+
+#Basic Cleaning (Birthdate and Age, ID as a number)
+dataCleaning <- data %>%
+  mutate(Birthdate = ifelse(Birthdate == "0001-01-01", NA, Birthdate)) %>%
+  mutate(Birthdate = mdy(Birthdate)) %>%
+  mutate(Age = as.numeric(floor(interval(start= Birthdate, end=Sys.Date())/duration(n=1, unit="years")))) %>%
+  mutate(Spouse.Birthdate = ifelse(Spouse.Birthdate == "0001-01-01", NA, Spouse.Birthdate)) %>%
+  mutate(Spouse.Birthdate = mdy(Spouse.Birthdate)) %>%
+  mutate(Spouse.Age = as.numeric(floor(interval(start= Spouse.Birthdate,
+                                                end=Sys.Date())/duration(n=1, unit="years")))) %>%
+  mutate(ID = as.numeric(ID))
 
 
+donor_data_tidy <- dataCleaning %>%
+  mutate(Category.Codes = strsplit(as.character(Category.Codes), "|", fixed = TRUE)) %>%
+  unnest(Category.Codes)
